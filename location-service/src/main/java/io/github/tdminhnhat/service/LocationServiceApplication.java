cb9515d5bae48bcd4317c3986bdc95ad21d3920e
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-09-18 14:59:52 +0700

    Add component scan

--- location-service/src/main/java/io/github/tdminhnhat/service/LocationServiceApplication.java
+++ location-service/src/main/java/io/github/tdminhnhat/service/LocationServiceApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@ComponentScan(basePackages = {"io.github.tdminhnhat"})
 public class LocationServiceApplication {
 
     static void main(String[] args) {
