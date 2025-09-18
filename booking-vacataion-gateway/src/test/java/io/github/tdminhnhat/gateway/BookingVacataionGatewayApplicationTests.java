b9fce1e568a12e89f5382f34c78be9d476a09e98
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-09-18 09:45:09 +0700

    Init gateway

---
+++ booking-vacataion-gateway/src/test/java/io/github/tdminhnhat/gateway/BookingVacataionGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tdminhnhat.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingVacataionGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
