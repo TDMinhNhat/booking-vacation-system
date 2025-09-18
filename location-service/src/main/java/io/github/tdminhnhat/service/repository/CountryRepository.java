8b72728a9fe169d737609b035e8124a4f5912fad
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-09-18 13:08:05 +0700

    Fix syntax query

--- location-service/src/main/java/io/github/tdminhnhat/service/repository/CountryRepository.java
+++ location-service/src/main/java/io/github/tdminhnhat/service/repository/CountryRepository.java
@@ -14,12 +14,12 @@ public interface CountryRepository extends JpaRepository<Country, Long> {
 
     @Query("""
         SELECT c FROM Country c
-        WHERE (#{#filter.id} IS NULL OR #{#filter.id} = c.id)
-        AND (#{#filter.countryCode} IS NULL OR #{#filter.countryCode} = c.countryCode)
-        AND (#{#filter.countryName} IS NULL OR #{#filter.countryName} = c.countryName)
-        AND (#{#filter.delete} IS NULL OR #{#filter.delete} = c.delete)
-        AND (#{#filter.beginCreatedAt} IS NULL OR #{#filter.endCreatedAt} IS NULL OR (c.createdAt >= #{#filter.beginCreatedAt} AND c.createdAt <= #{#filter.endCreatedAt}))
-        AND (#{#filter.beginUpdatedAt} IS NULL OR #{#filter.endUpdatedAt} IS NULL OR (c.createdAt >= #{#filter.beginUpdatedAt} AND c.createdAt <= #{#filter.endUpdatedAt}))
+        WHERE (:#{#filter.id} IS NULL OR :#{#filter.id} = c.id)
+        AND (:#{#filter.countryCode} IS NULL OR :#{#filter.countryCode} = c.countryCode)
+        AND (:#{#filter.countryName} IS NULL OR :#{#filter.countryName} = c.countryName)
+        AND (:#{#filter.delete} IS NULL OR :#{#filter.delete} = c.delete)
+        AND (:#{#filter.beginCreatedAt} IS NULL OR :#{#filter.endCreatedAt} IS NULL OR (c.createdAt >= :#{#filter.beginCreatedAt} AND c.createdAt <= :#{#filter.endCreatedAt}))
+        AND (:#{#filter.beginUpdatedAt} IS NULL OR :#{#filter.endUpdatedAt} IS NULL OR (c.createdAt >= :#{#filter.beginUpdatedAt} AND c.createdAt <= :#{#filter.endUpdatedAt}))
         """)
     Page<Country> getCountriesByFilter(@Param("filter") CountryQo filter, Pageable pageable);
 
