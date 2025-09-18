cb13ef72db9ccd9735d564340108b9ab9a98fc7c
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-09-18 09:13:33 +0700

    Init eureka server

---
+++ booking-vacation-server/src/main/java/io/github/tdminhnhat/server/BookingVacationServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.tdminhnhat.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingVacationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingVacationServerApplication.class, args);
+    }
+
+}
---
+++ booking-vacation-server/src/test/java/io/github/tdminhnhat/server/BookingVacationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tdminhnhat.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingVacationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
