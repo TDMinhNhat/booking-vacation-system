404f64853d4a8680961cbbb524ff9e240971ff9a
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-09-18 09:57:10 +0700

    Init library core

---
+++ booking-vacation-core/src/test/java/io/github/tdminhnhat/core/BookingVacationCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tdminhnhat.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingVacationCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
