24f8e7ec907cf9af3a7fe33fe461d210f16e027f
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-09-19 14:29:33 +0700

    Init stay service

---
+++ stay-service/src/main/java/io/github/tdminhnhat/service/StayServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.tdminhnhat.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StayServiceApplication.class, args);
+    }
+
+}
