d32a370b68aefde9658b5516c13af521bfdca837
TDMinhNhat <tdminhnhat13092003@gmail.com>
2025-09-22 10:06:44 +0700

    Add mapping routes

--- booking-vacataion-gateway/src/main/java/io/github/tdminhnhat/gateway/config/GatewayRoutesConfiguration.java
+++ booking-vacataion-gateway/src/main/java/io/github/tdminhnhat/gateway/config/GatewayRoutesConfiguration.java
@@ -11,6 +11,10 @@ public class GatewayRoutesConfiguration {
     @Bean
     public RouteLocator configRouteLocators(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("location-service", r -> r.path("/location/**").uri("lb://location-service")).build();
+                .route("location-service", r -> r.path("/location/**").uri("lb://location-service"))
+                .route("facility-service", r -> r.path("/facility/**").uri("lb://facility-service"))
+                .route("stay-service", r -> r.path("/stay/**").uri("lb://stay-service"))
+                .route("user-service", r -> r.path("/user/**").uri("lb://user-service"))
+                .build();
     }
 }
